33e68c9a0116dc84b49c0bfa4f54a6f1c71d2ae0
yagolscpt <yagoprograming@gmail.com>
2022-05-11 13:29:21 +0200

    first commit

---
+++ demo/src/main/java/com/example/demo/ProyectoYagoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoYagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoYagoApplication.class, args);
+	}
+
+}
